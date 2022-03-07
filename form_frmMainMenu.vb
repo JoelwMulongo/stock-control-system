9496d148e022fc23404df02b088ed453015e46fd
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-03-07 20:55:51 +0300

    Update form_frmMainMenu.vb

--- form_frmMainMenu.vb
+++ form_frmMainMenu.vb
@@ -1,17 +1,14 @@
 Option Compare Database
-
 Private Sub btnClose_Click()
     'Exit Database
     DoCmd.Quit
 End Sub
-
 Private Sub btnOrdersMenu_Click()
     'Open Orders Menu
     DoCmd.OpenForm "frmOrdersMenu", acNormal
     'Close Main Menu
     DoCmd.Close acForm, "frmMainMenu"
 End Sub
-
 Private Sub btnStockMenu_Click()
     'Open Stock Menu
     DoCmd.OpenForm "frmStockMenu", acNormal
