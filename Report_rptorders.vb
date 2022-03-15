e825334f35cdbce88b26fd313b268f9b8984d8ec
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-03-16 00:14:53 +0300

    Update Report_rptorders.vb

--- Report_rptorders.vb
+++ Report_rptorders.vb
@@ -1,10 +1,8 @@
 Option Compare Database
-
-Private Sub btnClose_Click()
+private Sub btnClose_Click()
     'Close the report
     DoCmd.Close acReport, "rptOrders"
 End Sub
-
 Private Sub lblDetails_Click()
     'Declare variables to be used
     Dim DocName As String 'name for document to be opened
